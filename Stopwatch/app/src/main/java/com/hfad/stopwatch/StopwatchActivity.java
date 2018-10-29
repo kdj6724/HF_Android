e55345ddd30e4ea55ed965f191d2b6e5a3d850d7
김동진 <kdj6724@gmail.com>
2018-10-29 23:47:38 +0900

    Add onStop and onStart

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 public class StopwatchActivity extends Activity {
     private int seconds = 0;
     private boolean running = false;
+    private boolean wasRunning = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +19,7 @@ public class StopwatchActivity extends Activity {
         if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
     }
@@ -52,5 +54,16 @@ public class StopwatchActivity extends Activity {
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    protected void onStop() {
+        super.onStop();
+        running = false;
+        wasRunning = true;
+    }
+    protected void onStart() {
+        super.onStart();
+        if (wasRunning == true)
+            running = true;
     }
 }
