a060ff1c24e1fae3909b892dee07df038a0a11fa
김동진 <kdj6724@gmail.com>
2018-11-07 17:26:13 +0900

    Add fragment small size

---
+++ Workout/app/src/main/java/com/hfad/workout/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hfad.workout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+    implements WorkoutListFragment.Listener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+    public void onShowDetails(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
+
+    public void itemClicked(long id) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+        startActivity(intent);
+    }
+}
