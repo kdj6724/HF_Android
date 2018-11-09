8774592bffc79853d3aed4c60cb9313b71d3ac94
김동진 <kdj6724@gmail.com>
2018-11-09 17:00:30 +0900

    Add Fragment 안에 Fragment

--- Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -3,6 +3,7 @@ package com.hfad.workout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +22,18 @@ public class WorkoutDetailFragment extends Fragment {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null)
+        if (savedInstanceState == null) {
+            if (savedInstanceState == null) {
+                StopwatchFragment stopwatch = new StopwatchFragment();
+                FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+                ft.add(R.id.stopwatch_container, stopwatch);
+                ft.addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        } else {
             workoutId = savedInstanceState.getLong("workoutId");
+        }
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
